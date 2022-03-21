16f93cb77c2623e0ad3da956b1cbdc042ce0e926
raflyarv <89493269+raflyarv@users.noreply.github.com>
2022-03-21 16:50:36 +0700

    Update Footer.jsx

--- src/components/layout/elements/Footer.jsx
+++ src/components/layout/elements/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className=" bg-white h-16 fixed bottom-0 left-0 w-full z-50">
       <div className="max-w-md mx-auto h-full border-t border-r border-l flex justify-center items-center space-x-4">
-        <Link href="/">
+        <Link href="/" passHref>
           <button type="button" className="inline-flex w-16 h-full justify-center items-center flex-col border-t-4 border-cyan-500 text-cyan-500">
             <HomeIcon className="w-6 h-6" />
             <span className="text-sm">
@@ -14,7 +14,7 @@ const Footer = () => {
           </button>
         </Link>
 
-        <Link href="/upload">
+        <Link href="/upload" passHref>
           <button type="button" className="inline-flex w-16 h-full justify-center items-center flex-col border-t-4 border-cyan-500 text-cyan-500">
             <UploadIcon className="w-6 h-6" />
             <span className="text-sm">
@@ -23,7 +23,7 @@ const Footer = () => {
           </button>
         </Link>
 
-        <Link href="/account">
+        <Link href="/account" passHref>
           <button type="button" className="inline-flex w-16 h-full justify-center items-center flex-col border-t-4 border-cyan-500 text-cyan-500">
             <UserIcon className="w-6 h-6" />
             <span className="text-sm">
